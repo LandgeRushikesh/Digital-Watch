3c3f0f1912fbc867d9be31e3f5836f9d84fa1a02
LandgeRushikesh <rushilandge12@gmail.com>
2024-11-03 15:38:40 +0530

    Added toggle button

--- script.js
+++ script.js
@@ -3,29 +3,38 @@ let day = document.querySelector(".day")
 let time = document.querySelector(".time")
 let indicator = document.querySelector("#indicator")
 let colon = document.querySelector(".colon")
+let btn = document.querySelector(".change")
 
 // for updating time,date,day
 setInterval(()=>{
     const now = new Date()
-    getTime(now.getHours(),now.getMinutes(),now.getSeconds())
+    if(btn.classList.contains("12hrs")){
+        setTime12hrs(now.getHours(),now.getMinutes(),now.getSeconds())
+    }
+    else{
+        setTime24hrs(now.getHours(),now.getMinutes(),now.getSeconds())
+    }
     getDate(now.getDate(),now.getMonth(),now.getFullYear())
     getDay(now.getDay())
 },1000)
 
-// for blinking of the colon
-setInterval(()=>{
-    colon.classList.toggle("blink")
-},500)
-
 // time function
-let getTime = (hour,min,sec) =>{
+let setTime12hrs = (hour,min,sec) =>{
     let d = ""
     hour<12?d="AM":d="PM"
     hour = hour%12//converting 24 hrs clock into 12 hrs clock
+    hour = hour === 0 ? 12 : hour; // Adjust to show 12 instead of 0
     sec = sec<10 ? '0' + sec : sec
     min = min<10 ? '0' + min : min
     hour = hour<10 ? '0' + hour : hour
     let str = `${hour}<span class="colon">:</span>${min}<span class="colon">:</span>${sec} <span id="indicator">${d}</span>`
+    time.innerHTML = str
+}
+let setTime24hrs = (hour,min,sec) =>{
+    sec = sec<10 ? '0' + sec : sec
+    min = min<10 ? '0' + min : min
+    hour = hour<10 ? '0' + hour : hour
+    let str = `${hour}<span class="colon">:</span>${min}<span class="colon">:</span>${sec}`
     time.innerHTML = str
 }
 
@@ -65,3 +74,16 @@ let days = {
 let getDay = (today)=>{
     day.innerText = days[today]
 }
+
+// Adding Event listner to button
+
+btn.addEventListener("click",()=>{
+    if(btn.classList.contains("12hrs")){
+        btn.innerText = "Switch to 12-Hour Format"
+        btn.classList.remove("12hrs")
+    }
+    else{
+        btn.innerText = "Switch to 24-Hour Format"
+        btn.classList.add("12hrs")
+    }
+})
